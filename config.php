675dd5422d49fe31b2ebeff83089d14d1acdcb36
Raznation <TheEndi@gmx.de>
2018-01-15 18:48:55 +0100

    DB Connection
    Datenbank korrektur & kleines Fehler updatet

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 //DatenBank connecten
 try {
-    $dbc=new PDO("mysql:host=localhost;dbname=databasename","username","password")
+    $dbc=new PDO("mysql:host=193.196.143.168;dbname=mm7w_62fuch1bif","mm7w_62fuch1bif","bla12345")
     or die("Unable to connect.");
 }
 catch(PDOException $e)
